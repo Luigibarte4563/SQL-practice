93ca557dc9e5398d9918d9ef48e64e1f456913a2
Luigibarte4563 <luigibarte1@gmail.com>
2026-01-17 22:07:40 +0800

    READ

---
+++ READ/SELECT.sql
@@ -0,0 +1,8 @@
+/*Selecting the table*/
+SELECT * FROM tblemployees;
+
+/*Selecting using LastName*/
+SELECT * FROM tblemployees WHERE LastName = "Santos";
+
+/*Conditional Operators*/
+SELECT * FROM tblemployees WHERE Age >= 30;
