a0f1529c1828701ebefa1db1fa1bd5f64ab0688d
Luigibarte4563 <luigibarte1@gmail.com>
2026-01-17 09:44:20 +0800

    CreateDatabase-CreateTable-InsertInto

---
+++ CreateDatabase-CreateTable-InsertInto/CreateDatabase-CreateTable-InsertIntoC.sql
@@ -0,0 +1,32 @@
+/*create database dbskol;*/
+
+/*create table tblStudents(
+	SID int primary key auto_increment,
+    FirstName varchar(16) not null,
+    LastName varchar(16) not null,
+    PhoneNumber varchar(11),
+    Schoolarship bool not null,
+    DateEnrolled date not null
+);*/
+
+
+
+INSERT INTO tblstudents (
+	FirstName,
+    LastName,
+    PhoneNumber,
+    Schoolarship,
+    DateEnrolled
+) VALUES (
+	"Luigi",
+    "Barte",
+    "09334555678",
+    TRUE,
+    "2026-01-05"
+), (
+	"Lowi",
+    "Bart",
+    "09456837464",
+    FALSE,
+    "2026-01-10"
+)
