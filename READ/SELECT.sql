692eabfc1eeeee5c22fdec88abe0b4313a74c130
Luigibarte4563 <luigibarte1@gmail.com>
2026-01-17 22:09:14 +0800

    NOT keyword

--- READ/SELECT.sql
+++ READ/SELECT.sql
@@ -6,3 +6,6 @@ SELECT * FROM tblemployees WHERE LastName = "Santos";
 
 /*Conditional Operators*/
 SELECT * FROM tblemployees WHERE Age >= 30;
+
+/*Selecting all Employess EXCEPT if their Last Name is "Santos"*/
+SELECT * FROM tblemployees WHERE NOT LastName = "Santos";
