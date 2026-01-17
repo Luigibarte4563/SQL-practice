9b58de50de3f15f4e3a22821394b5aeae2131298
Luigibarte4563 <luigibarte1@gmail.com>
2026-01-17 22:30:14 +0800

    Logical Operator is added

--- READ/SELECT.sql
+++ READ/SELECT.sql
@@ -7,5 +7,14 @@ SELECT * FROM tblemployees WHERE LastName = "Santos";
 /*Conditional Operators*/
 SELECT * FROM tblemployees WHERE Age >= 30;
 
-/*Selecting all Employess EXCEPT if their Last Name is "Santos"*/
+/*Select ALL Employess EXCEPT if their Last Name is "Santos"*/
 SELECT * FROM tblemployees WHERE NOT LastName = "Santos";
+
+/*Select ALL Employees with the Last Name "Santos" and "Dela Cruz"*/
+SELECT * FROM tblemployees WHERE LastName IN ("Santos", "Cruz");
+
+/*Slect ALL Employees with the Last Name "Santos" and their age must be 30 and above*/
+SELECT * FROM tblemployees WHERE LastName = "Santos" AND Age >= 18;
+
+/*Select All Employees with the Last Name "Santos" or their age must be 30 and above*/
+SELECT * FROM tblemployees WHERE LastName = "Santos" OR Age >= 18;
